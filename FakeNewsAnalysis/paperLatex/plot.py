f4eeccb8c5b4b0c7dc3283a5bb8e011302ebd3d4
feupos <feupos@hotmail.com>
2019-09-04 20:46:02 -0300

    improved plot and text

--- FakeNewsAnalysis/paperLatex/plot.py
+++ FakeNewsAnalysis/paperLatex/plot.py
@@ -3,14 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-data = [['Original Dataset', 'Original Features (Title)', 1],['Original Dataset', 'Expanded Features (Title)',2],
-        ['Original Dataset', 'Original Features (Body)', 1],['Original Dataset', 'Expanded Features (Title)',2],
-        ['Original Dataset', 'Original Features (Both)', 1],['Original Dataset', 'Expanded Features (Title)',2],
-        ['Expanded Dataset', 'Original Features (Title)', 3],['Expanded Dataset', 'Expanded Features (Title)',4],
-        ['Expanded Dataset', 'Original Features (Body)', 3],['Expanded Dataset', 'Expanded Features (Title)',4],
-        ['Expanded Dataset', 'Original Features (Both)', 3],['Expanded Dataset', 'Expanded Features (Title)',4]]
-df = pd.DataFrame(data,columns=['Dataset','Feature Set','Accuracy'],dtype=float)
-sns.barplot(x="Accuracy", y="Feature Set", hue="Dataset", data=df);
-
+data = [['Original', 'Original', 'Title', 72.67],['Original', 'Expanded', 'Title',71.33],
+        ['Original', 'Original', 'Body', 78.67],['Original', 'Expanded', 'Body',82],
+        ['Original', 'Original', 'Combined', 80],['Original', 'Expanded', 'Combined',82],
+        ['Expanded', 'Original', 'Title', 56.26],['Expanded', 'Expanded', 'Title', 61.93],
+        ['Expanded', 'Original', 'Body', 62.19],['Expanded', 'Expanded', 'Body', 73.93],
+        ['Expanded', 'Original', 'Combined', 61.48],['Expanded', 'Expanded', 'Combined', 71.8]]
+df = pd.DataFrame(data,columns=['Dataset','Features', 'Feature Set','Accuracy'])
+#sns.barplot(x='Accuracy', y='Dataset', hue='Feature Set', data=df, orient='v')
+g = sns.catplot(x="Features", y="Accuracy", hue="Feature Set", col="Dataset", data=df, kind="bar", height=4, aspect=.7, palette="Blues")
+g.set(ylim=(50, 85))
 plt.show()
